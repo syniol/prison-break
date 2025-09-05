a410e221014d06bddff39af551bf92ed67e155fd
Hadi T <hadi@syniol.com>
2025-09-05 10:26:15 +0100

    added docs for exported methods

--- prison.go
+++ prison.go
@@ -5,8 +5,10 @@ import (
 	"time"
 )
 
+// InmateIPAddr is a specific string type for Clients (Prisoners) IP Address
 type InmateIPAddr string
 
+// PrisonInmate is definition of prisoner in each cell defined in Prison
 type PrisonInmate struct {
 	IP                     InmateIPAddr
 	Count                  int
@@ -15,11 +17,18 @@ type PrisonInmate struct {
 	LastInspectionDateTime time.Time
 }
 
+// Prison is a core domain that contains cells where key is an IP address with inmates information attached and rules
+// cells is
+// rules is
 type Prison struct {
 	cells map[string]*PrisonInmate
 	rules *PrisonRules
 }
 
+// PrisonRules defines the set of rules to be utilised for: isolation eligibility and prison cells clean up
+// IsolationRedLineCount is
+// IsolationRedLineDuration is
+// PrisonBreakDuration is
 type PrisonRules struct {
 	IsolationRedLineCount    int
 	IsolationRedLineDuration time.Duration
@@ -29,6 +38,11 @@ type PrisonRules struct {
 var once sync.Once
 var instance *Prison
 
+// NewPrison will create a new instance which accept a configuration called PrisonRules
+// rules are optional by default PrisonRules are:
+// IsolationRedLineCount:    20,
+// IsolationRedLineDuration: time.Second * 5,
+// PrisonBreakDuration:      time.Second * 10,
 func NewPrison(rules *PrisonRules) *Prison {
 	once.Do(func() {
 		instance = &Prison{
@@ -92,10 +106,14 @@ func (p *Prison) isolationEligibility(inmate *PrisonInmate) *PrisonInmate {
 	return inmate
 }
 
+// IsIsolated examines the criteria against PrisonRules
+// It determines the eligibility of Solidarity Confinement
 func (p *Prison) IsIsolated(ip string) bool {
 	return p.isolationEligibility(p.imprison(ip)).Isolated
 }
 
+// Torture executes IsIsolated and the call back method defined as argument
+// You can find an example of this inside the documentation and the example_test.go
 func (p *Prison) Torture(ip string, cb func() error) error {
 	if p.IsIsolated(ip) {
 		return cb()
