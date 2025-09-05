9b14bdf47c6a6e371dc7e3ae8433396265d2ab2d
Hadi T <hadi@syniol.com>
2025-09-05 21:30:50 +0100

    remove dthe count

--- break.go
+++ break.go
@@ -10,11 +10,8 @@ func prisonBreak(prison *Prison) {
 		// clean up cache token every prison.rules.PrisonBreakDuration + time.Second
 		cachePrisonCellTicker := time.NewTicker(prison.rules.PrisonBreakDuration + time.Millisecond)
 		for _ = range cachePrisonCellTicker.C {
-			var count = 0
-
 			for i, v := range prison.cells {
 				if v.LastInspectionDateTime.Sub(time.Now()) >= prison.rules.PrisonBreakDuration {
-					count++
 					delete(prison.cells, i)
 				}
 			}
