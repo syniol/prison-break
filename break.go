5d0cc16d88567699da65208f92e2ea11f1fa24ac
Hadi T <hadi@syniol.com>
2025-09-06 15:19:54 +0100

    attempt to resolve the race cond

--- break.go
+++ break.go
@@ -9,12 +9,13 @@ import (
 func prisonBreak(ctx context.Context, prison *Prison) {
 	cachePrisonCellTicker := time.NewTicker(prison.rules.PrisonBreakDuration + time.Millisecond)
 
-	go func(prison *Prison, ticker *time.Ticker, ctx context.Context) {
+	go func(prison *Prison, cachePrisonCellTicker *time.Ticker, ctx context.Context) {
 		select {
 		case <-ctx.Done():
+			cachePrisonCellTicker.Stop()
 			return
 		default:
-			for range ticker.C {
+			for range cachePrisonCellTicker.C {
 				for i, v := range prison.cells {
 					if time.Now().Sub(v.LastInspectionDateTime) >= prison.rules.PrisonBreakDuration {
 						delete(prison.cells, i)
