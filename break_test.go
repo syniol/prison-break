0bbdb112be0b436ac7ca0275e4d782e58fe25a71
Hadi T <hadi@syniol.com>
2025-09-06 01:30:16 +0100

    Updatd the test

--- break_test.go
+++ break_test.go
@@ -9,16 +9,29 @@ func TestPrisonBreak(t *testing.T) {
 	prison := NewPrison(nil)
 
 	t.Run("when duration between LastInspectedDateTime and PrisonBreakDuration is greater", func(t *testing.T) {
-		t.Log(len(prison.cells))
+		if len(prison.cells) != 0 {
+			t.Errorf("prisonBreak should be empty")
+		}
+
 		prison.imprison("127.0.0.1")
-		t.Log(len(prison.cells))
+		if len(prison.cells) != 1 {
+			t.Errorf("prisonBreak should have a new inmate")
+		}
+
 		time.Sleep(defaultPrisonBreakDuration + time.Millisecond*2)
-		t.Log(len(prison.cells))
+		if len(prison.cells) != 0 {
+			t.Errorf("prisonBreak should be empty")
+		}
+
+		prison.imprison("127.0.0.1")
+		if len(prison.cells) != 1 {
+			t.Errorf("prisonBreak should have a new inmate")
+		}
+
+		time.Sleep(defaultPrisonBreakDuration - time.Nanosecond)
+		if len(prison.cells) != 1 {
+			t.Errorf("prisonBreak should have a new inmate")
+		}
 	})
 
-	//t.Run("when duration between LastInspectedDateTime and PrisonBreakDuration is less", func(t *testing.T) {
-	//	t.Log(len(prison.cells))
-	//	prison.imprison("127.0.0.2")
-	//	t.Log(len(prison.cells))
-	//})
 }
