689d4adf74c5d881def6d81c29979e0c5b6f4bcb
Hadi T <hadi@syniol.com>
2025-09-06 11:04:52 +0100

    removed uneccessery defined for loop

--- break.go
+++ break.go
@@ -5,7 +5,7 @@ import "time"
 // prisonBreak will free the inmates based on criteria defined in configuration
 func prisonBreak(prison *Prison) {
 	cachePrisonCellTicker := time.NewTicker(prison.rules.PrisonBreakDuration + time.Millisecond)
-	for _ = range cachePrisonCellTicker.C {
+	for range cachePrisonCellTicker.C {
 		for i, v := range prison.cells {
 			if time.Now().Sub(v.LastInspectionDateTime) >= prison.rules.PrisonBreakDuration {
 				delete(prison.cells, i)
